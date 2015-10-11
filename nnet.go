ef4bac8e65cfe9695f0ff002fc2a5c0c59f76ffd
Tatsuro Alpert <talpert@newrelic.com>
2015-10-11 13:34:31 -0700

    use gridnet

--- nnet.go
+++ nnet.go
@@ -4,13 +4,14 @@ import (
 	log "github.com/Sirupsen/logrus"
 	"gopkg.in/alecthomas/kingpin.v2"
 	"os"
+	"source.datanerd.us/talpert/nnet/gridnet"
 	"source.datanerd.us/talpert/nnet/simplenet"
 	"time"
 )
 
 type Network interface {
-	New(int)
-	Run()
+	New(int, [][][]float64)
+	Run([]float64) (float64, error)
 }
 
 func init() {
@@ -37,23 +38,40 @@ func main() {
 	// 	{{0.5, 0.5}, {0.5, 0.5}},
 	// 	{{0.5, 0.5}},
 	// }
-	net, err := simplenet.New(3, [][][]float64{
+	weights := [][][]float64{
 		{{0.1, 0.2, 0.0}, {0.3, 0.4, 0.1}},
 		{{0.1, 0.2}, {0.3, 0.4}, {0.5, 0.6}},
 		{{0.7, 0.8, 0.9}},
-	})
+	}
+	snet, err := simplenet.New(3, weights)
 	if err != nil {
 		log.Fatal(err)
 	}
 	for _, inputVals := range sampleData {
 		start := time.Now()
 		log.Infof("Inputs: %v", inputVals)
-		ret, err := net.Run(inputVals)
+		ret, err := snet.Run(inputVals)
 		if err != nil {
 			log.Fatal(err)
 		}
 		log.Infof("Result: %f", ret)
-		log.Infof("Time: %v", time.Since(start))
+		log.Infof("SimpleNet Time: %v", time.Since(start))
 	}
+
+	gnet, gErr := gridnet.New(3, weights)
+	if gErr != nil {
+		log.Fatal(err)
+	}
+	for _, inputVals := range sampleData {
+		start := time.Now()
+		log.Infof("Inputs: %v", inputVals)
+		ret, err := gnet.Run(inputVals)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Infof("Result: %f", ret)
+		log.Infof("GridNet Time: %v", time.Since(start))
+	}
+
 	os.Exit(0)
 }
